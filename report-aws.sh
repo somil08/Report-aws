70eb4c9a6ae262a02cd9844acf778f6933ef7a72
Ubuntu <ubuntu@ip-172-31-77-106.ec2.internal>
2024-09-17 11:12:45 +0000

    1st update

---
+++ report-aws.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+###################################
+#Author: Somil Rathore
+# Date: 17th-sep 
+# version: v1 
+# this script will report the AWS resource usage
+###################################
